218bc5d002eeaea0bfd0aa75dd4ebddfa14fa158
irisri <irisrifold@gmail.com>
2023-05-27 19:00:41 +0300

    add toggel button

--- src/components/carouselItem.tsx
+++ src/components/carouselItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
 
@@ -10,10 +10,16 @@ import { Travalformation } from './travalInformatio';
 import { DataTag } from './dateTag';
 
 import arrow from '../assets/arrow.svg';
+import back from '../assets/back.svg';
 import pic0 from '../assets/pic0.png';
 import pic1 from '../assets/pic1.png';
 import pic2 from '../assets/pic2.png';
 import pic3 from '../assets/pic3.png';
+
+const enum ViewProfile {
+  Profile = 'profile',
+  Travel = 'travel',
+}
 
 interface StylePropsRootCarouse {
   imgSrc: string;
@@ -24,7 +30,6 @@ const RootCarouselItem = styled.div`
   height: 700px;
   width: 430px;
   display: flex;
-  justify-content: center;
   background-image: url(${(props: StylePropsRootCarouse) => props.imgSrc});
   border-bottom-left-radius: ${(props: StylePropsRootCarouse) => (props.isUserProfile ? '60px 60px' : 0)};
   border-bottom-right-radius: ${(props: StylePropsRootCarouse) => (props.isUserProfile ? '60px 60px' : 0)};
@@ -79,12 +84,59 @@ const FotterButton = styled.button`
   cursor: pointer;
 `;
 
+const ProfileDiv = styled.div`
+  margin-top: 28px;
+  margin-left: 19px;
+  display: flex;
+  align-items: center;
+`;
+
+const ToggoleDiv = styled.div`
+  background-color: #fff;
+  box-shadow: 0px 16px 22px rgba(0, 0, 0, 0.37);
+  border-radius: 17.5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  padding: 3px 2px;
+  gap: 9px;
+`;
+
+interface StyleProps {
+  isPicked: boolean;
+}
+
+const BackButton = styled.button`
+  height: 29px;
+  background: rgba(255, 255, 255, 0.2);
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  padding: 0;
+  margin-right: 100px;
+`;
+
+const ToggleButton = styled.button`
+  color: ${(props: StyleProps) => (props.isPicked ? '#fff' : '#878893')};
+  background-color: ${(props: StyleProps) => (props.isPicked ? '#7B33D7' : '#fff')};
+  border-radius: 17.5px;
+  padding: 7px 14px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+`;
+
 interface CarouselItemProps {
   index: number;
   isUserProfile: boolean;
 }
 
 export const CarouselItem = ({ index, isUserProfile }: CarouselItemProps) => {
+  const [pickedView, setPikedView] = useState(ViewProfile.Profile);
   let navigate = useNavigate();
 
   const imgList = [pic0, pic1, pic2, pic3];
@@ -140,7 +192,30 @@ export const CarouselItem = ({ index, isUserProfile }: CarouselItemProps) => {
             </FotterButton>
           </Footerdiv>
         </CarouselInformation>
-      ) : null}
+      ) : (
+        <div>
+          <ProfileDiv>
+            <BackButton>
+              <img src={back} alt='back' />
+            </BackButton>
+
+            <ToggoleDiv>
+              <ToggleButton
+                isPicked={pickedView === ViewProfile.Profile}
+                onClick={() => setPikedView(ViewProfile.Profile)}
+              >
+                Profile
+              </ToggleButton>
+              <ToggleButton
+                isPicked={pickedView === ViewProfile.Travel}
+                onClick={() => setPikedView(ViewProfile.Travel)}
+              >
+                My Travels
+              </ToggleButton>
+            </ToggoleDiv>
+          </ProfileDiv>
+        </div>
+      )}
     </RootCarouselItem>
   );
 };
